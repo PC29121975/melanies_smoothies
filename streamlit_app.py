86ba7d9217bbf57ae4d2c237e48f83093d356910
PC29121975 <patricia.coradini@gmail.com>
2026-01-22 13:52:42 -0300

    Update streamlit_app.py
    ddd

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,7 @@ if ingredientes_list:
         st.stop()
       
         st.subheader(fruit_chosen + 'Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/{search_on}")            
+        smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_on}")            
         st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     st.write(ingredients_string)
