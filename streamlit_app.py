10b70a2ec5f682eb01cc3a8b6175d890b39b4b4f
PC29121975 <patricia.coradini@gmail.com>
2026-01-22 12:30:21 -0300

    Update streamlit_app.py
    teste

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,8 +16,12 @@ st.write("The name of your Smoothie will be", name_on_order)
 cnx = st.connection("snowflake", type ="snowflake")
 session = cnx.session()
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('SEARCH_ON'))
+
+# Convert the Snowpark to Pandas
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop()
 
 ingredientes_list = st.multiselect(
     'Choose up to 5 ingredientes:'
@@ -31,8 +35,14 @@ if ingredientes_list:
 
     for fruit_chosen in ingredientes_list:
         ingredients_string += fruit_chosen + ' '
+
+        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The source value for ', fruit_chosen, ' is ', search_on, '.')
+      
         st.subheader(fruit_chosen + 'Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+
+             
         st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     st.write(ingredients_string)
