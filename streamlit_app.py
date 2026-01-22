45e44b79e8ad2e0514e34319abcf64b4a7974046
PC29121975 <patricia.coradini@gmail.com>
2026-01-22 13:38:03 -0300

    Update streamlit_app.py
    teste

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,9 +41,7 @@ if ingredientes_list:
         st.write('The source value for ', fruit_chosen, ' is ', search_on, '.')
       
         st.subheader(fruit_chosen + 'Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
-
-             
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/{search_on} )            
         st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     st.write(ingredients_string)
